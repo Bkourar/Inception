7d370d1621bdd33e7c9b4f30a293dc7a956510ee
bkk <sirachir@gmail.com>
2025-11-07 08:08:22 -0500

    complet just testing

--- srcs/requirements/mariadb/tools/set_updb.sh
+++ srcs/requirements/mariadb/tools/set_updb.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-cat << EOF > /etc/mysql/database_init.sql
+
+: "${db_name:=my_wp}"
+: "${db_user:=my_db}"
+: "${db_user_pass:=1234}"
+: "${MYSQL_ROOT_PASSWORD:=1234}"
+
+# Create the database_init.sql file in the correct location
+cat << EOF > /var/sql_data/database_init.sql
 CREATE DATABASE IF NOT EXISTS $db_name;
 CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_user_pass';
 GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%';
 FLUSH PRIVILEGES;
 EOF
+

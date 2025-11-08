b30c289a14b21470485fcbe8da031dee334750d5
bkk <sirachir@gmail.com>
2025-11-08 13:56:07 -0500

    optmize size images

--- srcs/requirements/mariadb/tools/set_updb.sh
+++ srcs/requirements/mariadb/tools/set_updb.sh
@@ -5,7 +5,6 @@
 : "${db_user_pass:=1234}"
 : "${MYSQL_ROOT_PASSWORD:=1234}"
 
-# Create the database_init.sql file in the correct location
 cat << EOF > /var/sql_data/database_init.sql
 CREATE DATABASE IF NOT EXISTS $db_name;
 CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_user_pass';

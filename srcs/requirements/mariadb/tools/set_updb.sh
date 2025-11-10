d5b8659304850b9a363aa4a1e31b94add1cd68a8
Bilal Kourar <bikourar@e3r11p10.1337.ma>
2025-11-10 18:31:27 +0100

    just check last commites

--- srcs/requirements/mariadb/tools/set_updb.sh
+++ srcs/requirements/mariadb/tools/set_updb.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-: "${db_name:=my_wp}"
-: "${db_user:=my_db}"
-: "${db_user_pass:=1234}"
-: "${MYSQL_ROOT_PASSWORD:=1234}"
-
 cat << EOF > /var/sql_data/database_init.sql
 CREATE DATABASE IF NOT EXISTS $db_name;
 CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_user_pass';
